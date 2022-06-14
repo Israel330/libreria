096baace0818086e0397a35d3f2a813fd86c8e8e
jonatan33 <jonatangeraldo33@hds>
2022-06-13 23:11:39 -0300

    libreria

---
+++ js/SwAlert.js
@@ -0,0 +1,7 @@
+
+
+
+ let search = document.getElementById("search")
+search.click = () => {Swal.fire('Any fool can use a computer')}
+search.onmousemove = () => {console.log("Move")};
+           
